6a13c0be1a0972dd19599d6b19139df06946957a
Leam Hall <leamhall@gmail.com>
2015-03-25 05:35:23 -0400

    Added the scripts to sort through the STIG titles.

--- tools/open_stig.rb
+++ tools/open_stig.rb
@@ -1,20 +1,26 @@
 #!/opt/puppet/bin/ruby
 #
-#
+# open_stig.rb
+# Version: 0.2
+# Used to read DISA STIG files and sort out the vulnerabilities.
+# Sorts by Vulnerability ID. For example, "V-12345". 
 #
 # So far the inputs are hard coded because the STIG XML file
-# headers seem difficult to munge. Working on that.
+# headers seem difficult to munge. Working on that. 
 #
 # TODO:
 #
-# 1. Deal with first three header lines.
-# 2. Be able to use command line option for input file.
-# 3. Output files that are useful to other programs and people.
+# 1. Deal with first three header lines. 
+#    Question is out to the Nokogiri group
+# 2. Be able to use command line option for input file. 
+# 3. Output files that are useful to other programs and people. 
 # 4. Maybe use newer versions of tools?
-# 5. Maybe use a stored array of Vulnerabilites so we can identify
+# 5. Maybe use a stored array of Vulnerabilites so we can identify 
 #    new ones?
 #
 
+require 'rubygems'
+require 'nokogiri'
 
 $vulns = Hash.new
 duplicate_vulns = Array.new
@@ -52,7 +58,7 @@ $vulns.each_key do | vul_id |
   id = vul_id
   sev = $vulns[vul_id]['severity']
   title = $vulns[vul_id]['title']
-  printf("%-8s %-8s %-100s \n", id, sev, title)
+  printf("%-8s %-8s %-10s \n", id, sev, title)
 end
 
 if duplicate_vulns.count > 0
